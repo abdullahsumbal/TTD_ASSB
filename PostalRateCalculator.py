56ecb8b75b071d9b3c13a4111a52e69aec8f3aae
abdullahsumbal <abdullahsumbal@gmail.com>
2018-03-03 17:14:19 -0800

    added nine tests

--- PostalRateCalculator.py
+++ PostalRateCalculator.py
@@ -1,13 +1,48 @@
 import sys
-
+import re
 
 class RateCalculator:
 
     def __init__(self, argv):
         self.argv = argv
+        self.len_arg = len(argv)
         self.cal()
 
     def cal(self):
-        pass
+        error_msg_less_args = "Usage: Python3 from_postal_code to_postal_code length width height weight post_type"
+        error_msg_more_args = "Usage: Python3 from_postal_code to_postal_code length width height weight post_type"
+        error_msg_from_arg = "Invalid From: Postal Code"
+        error_msg_to_arg = "Invalid To: Postal Code"
+        error_msg_post_type = "Invalid Post Type: [Regular, Xpress, Prior]"
 
-RateCalculator(sys.argv)
+        # validate the argument length
+        if self.len_arg < 7:
+            return error_msg_less_args
+        if self.len_arg > 7:
+            return error_msg_more_args
+
+        #validate from code_postal format
+        from_postal_code_arg = self.argv[0]
+        if (len(from_postal_code_arg) != 6):
+            return error_msg_from_arg
+        if (re.match("[YVTXSRPNMLKJGHECAB]{1}\d{1}[A-Z]{1}\d{1}[A-Z]{1}\d{1}", from_postal_code_arg)== None):
+            return error_msg_from_arg
+
+        #validate from code_postal format
+        to_postal_code_arg = self.argv[1]
+        if (len(to_postal_code_arg) != 6):
+            return error_msg_to_arg
+        if (re.match("[YVTXSRPNMLKJGHECAB]{1}\d{1}[A-Z]{1}\d{1}[A-Z]{1}\d{1}", to_postal_code_arg)== None):
+            return error_msg_to_arg
+
+
+        #validate post type
+        post_type = self.argv[6]
+        if (post_type not in ['Regular', 'Xpress', 'Prior']):
+            return error_msg_post_type
+        return
+
+if __name__ == '__main__':
+    sys.argv.pop(0)
+    print(RateCalculator(sys.argv))
+
--- tester.py
+++ tester.py
@@ -6,10 +6,48 @@ import sys
 
 class TestSolver(TestCase):
 
-    def no_args(self):
-        s = RateCalculator(sys.argv)
+    def test_no_args(self):
+        R = RateCalculator([])
+        self.assertEqual(R.cal(), "Usage: Python3 from_postal_code to_postal_code length width height weight post_type")
 
-        self.assertRaises(Exception, s.demo, 2, 1, 2)
+    def test_less_args(self):
+        R = RateCalculator(["H2w1j5"])
+        self.assertEqual(R.cal(), "Usage: Python3 from_postal_code to_postal_code length width height weight post_type")
 
-    # def test_demo(self):
-    #     self.fail()
+    def test_more_args(self):
+        R = RateCalculator([1,2,3,4,5,6,7,8])
+        self.assertEqual(R.cal(), "Usage: Python3 from_postal_code to_postal_code length width height weight post_type")
+
+    def test_correct_no_of_args(self):
+        R = RateCalculator(['','','','','','',''])
+        self.assertNotEqual(R.cal(), "Usage: Python3 from_postal_code to_postal_code length width height weight post_type")
+
+    def test_len_from_postal_code(self):
+        R = RateCalculator(["HeW1J534", 2, 3, 4, 5, 6, 7])
+        self.assertEqual(R.cal(),"Invalid From: Postal Code")
+
+    def test_wrong_from_postal_code_format(self):
+        R = RateCalculator(["HeW1J5",2,3,4,5,6,7])
+        self.assertEqual(R.cal(), "Invalid From: Postal Code")
+        R = RateCalculator(["D2W155",2,3,4,5,6,7])
+        self.assertEqual(R.cal(), "Invalid From: Postal Code")
+        R = RateCalculator(["H2W145",2,3,4,5,6,7])
+        self.assertEqual(R.cal(), "Invalid From: Postal Code")
+
+    def test_len_to_postal_code(self):
+        R = RateCalculator(["H2W1J5", "H2Wterer", 3, 4, 5, 6, 7])
+        self.assertEqual(R.cal(),"Invalid To: Postal Code")
+
+    def test_wrong_to_postal_code_format(self):
+        R = RateCalculator(["H2W1J5","H23335",3,4,5,6,7])
+        self.assertEqual(R.cal(), "Invalid To: Postal Code")
+        R = RateCalculator(["H2W1J5","D2W155",3,4,5,6,7])
+        self.assertEqual(R.cal(), "Invalid To: Postal Code")
+        R = RateCalculator(["H2W1J5","H2W145",3,4,5,6,7])
+        self.assertEqual(R.cal(), "Invalid To: Postal Code")
+
+    def test_wrong_post_type(self):
+        R = RateCalculator(["H2W1J5", "H2W1J5", 3, 4, 5, 6, "fjkldsf"])
+        self.assertEqual(R.cal(),"Invalid Post Type: [Regular, Xpress, Prior]")
+        R = RateCalculator(["H2W1J5", "H2W1J5", 3, 4, 5, 6, "Regular"])
+        self.assertNotEqual(R.cal(), "Invalid Post Type: [Regular, Xpress, Prior]")
